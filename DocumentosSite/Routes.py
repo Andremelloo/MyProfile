720709e6eafd5391c73abe64c0ca32b5d4726530
My Profile <andre_rmello@hotmail.com>
2021-12-07 00:45:50 -0300

    Initial commit

---
+++ DocumentosSite/Routes.py
@@ -0,0 +1,9 @@
+from flask import render_template
+from DocumentosSite import app
+
+
+
+
+@app.route ( "/" )
+def home():
+    return render_template('home.html')
---
+++ main.py
@@ -0,0 +1,5 @@
+from DocumentosSite import app
+
+if __name__== '__main__':
+
+    app.run(debug=True) # debug, faz atualizar automatico, sem precisar parar e dar RUN novamente
