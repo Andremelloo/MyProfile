3ee81b919b6f34d302fbe8556b6724084b70b0cd
My Profile <andre_rmello@hotmail.com>
2021-12-07 03:01:16 -0300

    commit V4

--- DocumentosSite/Routes.py
+++ DocumentosSite/Routes.py
@@ -7,3 +7,8 @@ from DocumentosSite import app
 @app.route ( "/" )
 def home():
     return render_template('home.html')
+
+
+@app.route ( "/about" )
+def about():
+    return render_template('about.html')
